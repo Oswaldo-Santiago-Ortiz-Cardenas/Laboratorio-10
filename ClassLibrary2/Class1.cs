4726761eac73cc2db460a6481ef8497ccfcda3d7
Oswaldo Santiago Ortiz Cardenas <143229490+Oswaldo-Santiago-Ortiz-Cardenas@users.noreply.github.com>
2023-10-24 22:31:39 -0500

    Add files via upload

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto.BibliotecaCode
+{
+
+    internal class Code
+    {
+        public static int Menu_principal()
+        {
+            Console.WriteLine("" +
+                "================================\n" +
+                "Productos de mPhone:\n" +
+                "================================\n" +
+                "1: Ventas de mPhone 3000\n" +
+                "2: Ventas de mPad 3500\n" +
+                "3: Ventas de MAPBrook 3800\n" +
+                "4: Ventas de mWatch 8000\n" +
+                "5:Salir\n" +
+                "================================\n" +
+                "Ingrese una opción:\n");
+            return opcion1();
+        }
+        public static int mphone_3000_1()
+        {
+            Console.WriteLine("" +
+                    "================================\n" +
+                    "Registrar Venta de mPhone 3000\n" +
+                    "================================\n" +
+                    "1: Registrar Venta\n" +
+                    "2: Registrar Devolución\n" +
+                    "3: Menu Principal\n" +
+                    "================================\n" +
+                    "Ingrese una opción:\n");
+            int opcion = opcion1();
+            return 0;
+        }
+        public static int mPad_3500_1()
+        {
+            Console.WriteLine("" +
+                    "================================\n" +
+                    "Registrar Venta de mPad 3500\n" +
+                    "================================\n" +
+                    "1: Registrar Venta\n" +
+                    "2: Registrar Devolución\n" +
+                    "3: Menu Principal\n" +
+                    "================================\n" +
+                    "Ingrese una opción:\n");
+            return opcion1();
+        }
+        public static int MAPBrook_3800_1()
+        {
+            Console.WriteLine("" +
+                "================================\n" +
+                "Registrar Venta de MAPBrook 3800\n" +
+                "================================\n" +
+                "1: Registrar Venta\n" +
+                "2: Registrar Devolución\n" +
+                "3: Menu Principal\n" +
+                "================================\n" +
+                "Ingrese una opción:\n");
+            return opcion1();
+        }
+        public static int mWatch_8000_1()
+        {
+            Console.WriteLine("" +
+                "================================\n" +
+                "Registrar Venta de mWatch 8000\n" +
+                "================================\n" +
+                "1: Registrar Venta\n" +
+                "2: Registrar Devolución\n" +
+                "3: Menu Principal\n" +
+                "================================\n" +
+                "Ingrese una opción:\n");
+            return opcion1();
+        }
+        public static int opcion1()
+        {
+            string opciontexto = Console.ReadLine();
+            Console.Clear();
+            return int.Parse(opciontexto);
+        }
+        public static void letra2()
+        {
+            Console.WriteLine("" +
+                "================================\n" +
+                "Registrar devolución de:\n" +
+                "================================\n" +
+                "Se va a registrar la devolución de\n");
+            return;
+        }
+        public static void letra2_1()
+        {
+            Console.WriteLine("" +
+                "1: Sí\n" +
+                "2: No\n" +
+                "================================\n" +
+                "Ingrese una opción:\n");
+            return;
+        }
+        public static void letras2_2_1()
+        {
+            Console.Write("" +
+                "================================\n" +
+                "Registrar venta de:\n" +
+                "================================\n" +
+                "Se va a registrar la venta de\n");
+            return;
+        }
+    }
+
+
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalVentasMPhones = 0;
+            int totaldevueltasmphones = 0;
+            int totalVentasMPads = 0;
+            int totaldevueltasmpads = 0;
+            int totalVentasMAPBrooks = 0;
+            int totaldevueltasmapbrooks = 0;
+            int totalVentasMWatches = 0;
+            int totaldevueltasmwatches = 0;
+            int opcion;
+            int opcion_1;
+            int opcion2;
+            int opcion2_1;
+
+            do
+            {
+                opcion = MenuPrincipal();
+                Console.Clear();
+
+                switch (opcion)
+                {
+                    case 1:
+                        MPhone_3000_1();
+                        opcion_1 = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        switch (opcion_1)
+                        {
+                            case 1:
+                                Console.WriteLine("Un mPhone 3000 ¿Desea Continuar?");
+                                Console.WriteLine("1: Sí");
+                                Console.WriteLine("2: No");
+                                opcion2 = int.Parse(Console.ReadLine());
+
+                                if (opcion2 == 1)
+                                {
+                                    totalVentasMPhones++;
+                                }
+
+                                Console.Clear();
+                                break;
+
+                            case 2:
+                                Console.WriteLine("Un mPhone 3000 ¿Desea Continuar?");
+                                Console.WriteLine("1: Sí");
+                                Console.WriteLine("2: No");
+                                opcion2_1 = int.Parse(Console.ReadLine());
+                                Console.Clear();
+
+                                if (opcion2_1 == 1)
+                                {
+                                    totaldevueltasmphones++;
+                                }
+                                break;
+                        }
+                        break;
+
+                    case 2:
+                        MPad_3500_1();
+                        opcion_1 = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        switch (opcion_1)
+                        {
+                            case 1:
+                                Console.WriteLine("Un mPad 3500 ¿Desea Continuar?");
+                                Console.WriteLine("1: Sí");
+                                Console.WriteLine("2: No");
+                                opcion2_1 = int.Parse(Console.ReadLine());
+                                Console.Clear();
+
+                                if (opcion2_1 == 1)
+                                {
+                                    totalVentasMPads++;
+                                }
+                                break;
+
+                            case 2:
+                                Console.WriteLine("Un mPad 3500 ¿Desea Continuar?");
+                                Console.WriteLine("1: Sí");
+                                Console.WriteLine("2: No");
+                                opcion2_1 = int.Parse(Console.ReadLine());
+                                Console.Clear();
+
+                                if (opcion2_1 == 1)
+                                {
+                                    totaldevueltasmpads++;
+                                }
+                                break;
+                        }
+                        break;
+
+                    case 3:
+                        MAPBrook_3800_1();
+                        opcion_1 = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        switch (opcion_1)
+                        {
+                            case 1:
+                                Console.WriteLine("Un MAPBrook 3800 ¿Desea Continuar?");
+                                Console.WriteLine("1: Sí");
+                                Console.WriteLine("2: No");
+                                opcion2 = int.Parse(Console.ReadLine());
+
+                                if (opcion2 == 1)
+                                {
+                                    totalVentasMAPBrooks++;
+                                }
+
+                                Console.Clear();
+                                break;
+
+                            case 2:
+                                Console.WriteLine("Un MAPBrook 3800 ¿Desea Continuar?");
+                                Console.WriteLine("1: Sí");
+                                Console.WriteLine("2: No");
+                                opcion2_1 = int.Parse(Console.ReadLine());
+                                Console.Clear();
+
+                                if (opcion2_1 == 1)
+                                {
+                                    totaldevueltasmapbrooks++;
+                                }
+                                break;
+                        }
+                        break;
+
+                    case 4:
+                        MWatch_8000_1();
+                        opcion_1 = int.Parse(Console.ReadLine());
+                        Console.Clear();
+
+                        switch (opcion_1)
+                        {
+                            case 1:
+                                Console.WriteLine("Un mWatch 8000 ¿Desea Continuar?");
+                                Console.WriteLine("1: Sí");
+                                Console.WriteLine("2: No");
+                                opcion2 = int.Parse(Console.ReadLine());
+
+                                if (opcion2 == 1)
+                                {
+                                    totalVentasMWatches++;
+                                }
+
+                                Console.Clear();
+                                break;
+
+                            case 2:
+                                Console.WriteLine("Un mWatch 8000 ¿Desea Continuar?");
+                                Console.WriteLine("1: Sí");
+                                Console.WriteLine("2: No");
+                                opcion2_1 = int.Parse(Console.ReadLine());
+                                Console.Clear();
+
+                                if (opcion2_1 == 1)
+                                {
+                                    totaldevueltasmwatches++;
+                                }
+                                break;
+
+                        }
+                        break;
+
+                    case 5:
+                        Console.WriteLine("==========ReporteFinal==========\n");
+                        Console.WriteLine("Productos Vendidos | Cantidad");
+                        Console.WriteLine("mPhones            | " + totalVentasMPhones);
+                        Console.WriteLine("mPads              | " + totalVentasMPads);
+                        Console.WriteLine("MAPBrooks          | " + totalVentasMAPBrooks);
+                        Console.WriteLine("mWatches           | " + totalVentasMWatches);
+                        int totalVentasProductos = totalVentasMPhones + totalVentasMPads + totalVentasMAPBrooks + totalVentasMWatches;
+                        Console.WriteLine("================================\n");
+                        Console.WriteLine("Total              | " + totalVentasProductos);
+                        Console.WriteLine("================================\n" +
+                            "¡Hasta Luego!\n");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            while (opcion != 5);
+        }
+
+        static int MenuPrincipal()
+        {
+            Console.WriteLine("Menú Principal:");
+            Console.WriteLine("1. Comprar mPhone 3000");
+            Console.WriteLine("2. Comprar mPad 3500");
+            Console.WriteLine("3. Comprar MAPBrook 3800");
+            Console.WriteLine("4. Comprar mWatch 8000");
+            Console.WriteLine("5. Salir");
+            Console.Write("Elija una opción: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void MPhone_3000_1()
+        {
+            Console.WriteLine("Menú mPhone 3000:");
+            Console.WriteLine("1. Comprar mPhone 3000");
+            Console.WriteLine("2. Devolver mPhone 3000");
+            Console.Write("Elija una opción: ");
+        }
+
+        static void MPad_3500_1()
+        {
+            Console.WriteLine("Menú mPad 3500:");
+            Console.WriteLine("1. Comprar mPad 3500");
+            Console.WriteLine("2. Devolver mPad 3500");
+            Console.Write("Elija una opción: ");
+        }
+
+        static void MAPBrook_3800_1()
+        {
+            Console.WriteLine("Menú MAPBrook 3800:");
+            Console.WriteLine("1. Comprar MAPBrook 3800");
+            Console.WriteLine("2. Devolver MAPBrook 3800");
+            Console.Write("Elija una opción: ");
+        }
+
+        static void MWatch_8000_1()
+        {
+            Console.WriteLine("Menú mWatch 8000:");
+            Console.WriteLine("1. Comprar mWatch 8000");
+            Console.WriteLine("2. Devolver mWatch 8000");
+            Console.Write("Elija una opción: ");
+        }
+    }
+}
